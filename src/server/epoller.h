036e00e6f70be0aab53f8021bf924981b1c966ea
Golorcsh <c1045132383@gmail.com>
2021-12-12 22:19:43 +0800

    添加Epoller类，该类封装了一些epoll的操作。

---
+++ src/server/epoller.h
@@ -0,0 +1,33 @@
+//
+// Created by chenshihao on 2021/12/12.
+//
+/*对epoll进行封装*/
+
+#ifndef WEBSERVER_SRC_SERVER_EPOLLER_H_
+#define WEBSERVER_SRC_SERVER_EPOLLER_H_
+
+#include <sys/epoll.h>/*epoll*/
+#include <fcntl.h>/*fcntl*/
+#include <unistd.h>/*close*/
+#include <cassert>
+#include <vector>
+#include <cerrno>
+
+class Epoller {
+ public:
+  explicit Epoller(int maxEvents = 1024);
+  ~Epoller();
+
+  bool AddFd(int fd, uint32_t events);
+  bool ModFd(int fd, uint32_t events);
+  bool DelFd(int fd, uint32_t events);
+  int Wait(int timeoutMs = -1);
+  int GetEventFd(size_t i) const;
+  uint32_t GetEvents(size_t i) const;
+
+ private:
+  int epollFd_;
+  std::vector<struct epoll_event> events_;
+};
+
+#endif //WEBSERVER_SRC_SERVER_EPOLLER_H_
